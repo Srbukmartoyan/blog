486ad7a5a2a90e207c37e44fb407ad2935e12043
Srbukmartoyan <srbukmartoyan@gmail.com>
2024-05-22 16:24:28 +0400

    integrating swr package in other components

--- frontend/src/pages/EditBlog.jsx
+++ frontend/src/pages/EditBlog.jsx
@@ -1,29 +1,20 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
+import useSWR from 'swr';
 import { useParams } from 'react-router-dom';
 import { BlogForm } from '../components';
 
+const fetcher = (...args) => fetch(...args).then(res => res.json());
+
 const EditBlog = () => {
   const { blogId } = useParams();
-  const [blogPost, setBlogPost] = useState({});
-  useEffect(() => {
-    const fetchBlogPost = async () => {
-      try {
-        const response = await fetch(`/posts/${blogId}`);
-        if (!response.ok) {
-          throw new Error('Failed to fetch blog post');
-        }
-        const post = await response.json();
-        setBlogPost(post);
-      } catch (error) {
-        console.error('Error fetching blog post:', error);
-      }
-    };
-    fetchBlogPost();
-  }, [blogId]);
+
+  const { data: blogPost, error } = useSWR(`/posts/${blogId}`, fetcher);
+  if (error) return <div>Failed to load blog post</div>;
+  if (!blogPost) return <div>Loading...</div>;
 
   return (
     <div className='w-full h-[92vh] bg-blue-100 flex justify-center items-center'>
-      {Object.keys(blogPost).length > 0 && <BlogForm blogPost={blogPost} />}
+      <BlogForm blogPost={blogPost} />
     </div>
   )
 }
