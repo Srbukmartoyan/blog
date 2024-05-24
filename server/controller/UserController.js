17d627be5b2a52ada6d958ed5eed4723fafe4740
Srbukmartoyan <srbukmartoyan@gmail.com>
2024-05-24 17:05:31 +0400

    refactoring user service and controller - deleting obect

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -1,32 +1,32 @@
-const UserService = require('../service/UserService.js')
+const { getUserPosts: getUserPostsService } = require('../service/UserService.js');
 
-const UserController = {
-    getAllPosts: async (req, res) => {
-        try {
-            const userId = req.user.id;
-            const posts = await UserService.getUserPosts(userId);
-            res.status(200).json(posts);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
-    },
-    getProfile: async (req, res) => {
-        try {
-            const user = req.user;
-            res.status(200).json(user);
-        } catch (error) {
-            res.status(500).json({ error: 'Failed to fetch user profile' });
-        } 
-    },
-    getAuthorPosts: async (req, res) => {
-        const { authorId } = req.params;
-        try {
-          const posts = await UserService.getUserPosts(authorId);
-          res.status(200).json(posts);
-        } catch (error) {
-          res.status(500).json({ error: 'Failed to fetch author\'s posts' });
-        }
+const getAllPosts = async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const posts = await getUserPostsService(userId);
+        res.status(200).json(posts);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const getProfile = async (req, res) => {
+    try {
+        const user = req.user;
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch user profile' });
+    }
+};
+
+const getAuthorPosts = async (req, res) => {
+    const { authorId } = req.params;
+    try {
+        const posts = await getUserPostsService(authorId);
+        res.status(200).json(posts);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch author\'s posts' });
     }
 }
 
-module.exports = UserController;
+module.exports = { getAllPosts, getProfile, getAuthorPosts };
