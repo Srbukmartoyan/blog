7f2f8ff92069909644788fa3aa5e174e5d7f8c0a
Srbukmartoyan <srbukmartoyan@gmail.com>
2024-06-05 10:30:03 +0400

    removeing myprofile from allUsers list

--- frontend/src/components/UserList/index.jsx
+++ frontend/src/components/UserList/index.jsx
@@ -1,11 +1,19 @@
 import ProfileCard from "../ProfileCard";
+import { fetcher } from "../../utils/fetcher";
+import useSWR from "swr";
 
 const UserList = ({users, title, showAction}) => {
+    const { data, error } = useSWR('/users/profile', fetcher);
+    let filteredUsers;
+
+    if (!data && !error) return <div>loading...</div>
+    error ? filteredUsers = users :  filteredUsers = users.filter((user) => user.id !== data.id);
+
     return (
         <div className="container mx-auto px-4 py-8">
             <h1 className="text-4xl font-bold mb-8 text-center text-gray-800">{title}</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-            {users.map((user, index) => {
+            {filteredUsers.map((user, index) => {
                return <ProfileCard 
                         key={index}
                         user={user}
