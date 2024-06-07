1969c0d1b2cd78a8fcbf06dc5dbb1cc4d48a145d
Srbukmartoyan <srbukmartoyan@gmail.com>
2024-06-07 17:34:50 +0400

    adding auth for user routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get('/', fetchUser, UserController.fetchAllUsers);
 router.get('/my/posts', fetchUser, UserController.fetchMyPosts);
-router.get('/:authorId/posts', UserController.fetchPostsByAuthorId);
+router.get('/:authorId/posts', fetchUser, UserController.fetchPostsByAuthorId);
 router.get('/:authorId/profile', fetchUser, UserController.fetchProfileByAuthorId);
 
 
