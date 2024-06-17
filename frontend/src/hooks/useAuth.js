e570c47576fffe6345e646dd60f0f42ee65a035b
Srbukmartoyan <srbukmartoyan@gmail.com>
2024-06-17 18:00:52 +0400

    completing token

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -20,7 +20,6 @@ const useAuth = () => {
         });
 
         if (response.ok) {
-          // console.log('hereeeeee', data.accessToken);
           const data = await response.json();
           setAccessToken(data.accessToken);
           localStorage.setItem('auth-token', data.accessToken);
@@ -54,50 +53,3 @@ const useAuth = () => {
 
 export default useAuth;
 
-
-
-// const useAuth = () => {
-//   const navigate = useNavigate();
-//   const token = localStorage.getItem('auth-token');
-
-//   if (!token) {
-//     navigate('/notfound');
-//   }
-
-//   return token;
-// };
-
-// export default useAuth;
-
-
-// const handleTokenRefresh = async (refreshToken) => {
-//   try {
-//       const response = await fetch('/refresh-token', {
-//           method: 'POST',
-//           headers: {
-//               'Content-Type': 'application/json',
-//           },
-//           body: JSON.stringify({ refreshToken }),
-//       });
-
-//       if (response.ok) {
-//           const data = await response.json();
-//           setAccessToken(data.accessToken);
-//           localStorage.setItem('auth-token', data.accessToken);
-//       } else {
-//           // Handle token refresh failure due to expired refresh token
-//           if (response.status === 401) {
-//               console.error('Refresh token has expired');
-//               // Clear tokens and redirect to login page
-//               localStorage.removeItem('auth-token');
-//               localStorage.removeItem('refresh-token');
-//               navigate('/login');
-//           } else {
-//               // Handle other errors
-//               console.error('Failed to refresh token:', response.statusText);
-//           }
-//       }
-//   } catch (error) {
-//       console.error('Failed to refresh token:', error);
-//   }
-// };
