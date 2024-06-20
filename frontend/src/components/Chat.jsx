11736114da8ab3873efc3c682efe17fbc49a927d
Srbukmartoyan <srbukmartoyan@gmail.com>
2024-06-20 16:51:33 +0400

    fixing nav ui

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,52 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import io from 'socket.io-client';
-import Message from './Message';
+// import React, { useState, useEffect } from 'react';
+// import io from 'socket.io-client';
+// import Message from './Message';
 
-const socket = io(process.env.REACT_APP_SOCKET_URL);
+// const socket = io(process.env.REACT_APP_SOCKET_URL);
 
-const Chat = () => {
-  const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState([]);
+// const Chat = () => {
+//   const [message, setMessage] = useState('');
+//   const [messages, setMessages] = useState([]);
 
-  useEffect(() => {
-    socket.on('receiveMessage', (message) => {
-      setMessages((prevMessages) => [...prevMessages, message]);
-    });
+//   useEffect(() => {
+//     socket.on('receiveMessage', (message) => {
+//       setMessages((prevMessages) => [...prevMessages, message]);
+//     });
 
-    return () => {
-      socket.off('receiveMessage');
-    };
-  }, []);
+//     return () => {
+//       socket.off('receiveMessage');
+//     };
+//   }, []);
 
-  const sendMessage = () => {
-    socket.emit('sendMessage', message);
-    setMessage('');
-  };
+//   const sendMessage = () => {
+//     socket.emit('sendMessage', message);
+//     setMessage('');
+//   };
 
-  return (
-    <div className="p-10 md:px-28 md:py-16 h-[92vh] flex flex-col items-center justify-center bg-slate-200">
-      <div className="w-full h-full overflow-y-scroll border border-gray-300 p-4 mb-4 bg-gray-50">
-        {messages.map((msg, index) => (
-          <Message key={index} text={msg} />
-        ))}
-      </div>
-      <div className="w-full flex">
-        <input
-          type="text"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          placeholder="Type your message..."
-          className="flex-grow p-2 border border-gray-300 rounded-l"
-        />
-        <button
-          onClick={sendMessage}
-          className="p-2 bg-slate-500 text-white rounded-r"
-        >
-          Send
-        </button>
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className="p-10 md:px-28 md:py-16 h-[92vh] flex flex-col items-center justify-center bg-slate-200">
+//       <div className="w-full h-full overflow-y-scroll border border-gray-300 p-4 mb-4 bg-gray-50">
+//         {messages.map((msg, index) => (
+//           <Message key={index} text={msg} />
+//         ))}
+//       </div>
+//       <div className="w-full flex">
+//         <input
+//           type="text"
+//           value={message}
+//           onChange={(e) => setMessage(e.target.value)}
+//           placeholder="Type your message..."
+//           className="flex-grow p-2 border border-gray-300 rounded-l"
+//         />
+//         <button
+//           onClick={sendMessage}
+//           className="p-2 bg-slate-500 text-white rounded-r"
+//         >
+//           Send
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Chat;
+// export default Chat;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,16 +2,18 @@ import { useState, useEffect } from 'react'
 import { NavLink } from "react-router-dom";
 
 const NavItem = ({ to, children }) => (
-  <li className='my-2'>
-    <NavLink
-      className='flex justify-between gap-2'
-      end
-      style={({ isActive }) => ({ color: isActive ? '#6A5ACD' : 'black' })}
-      to={to}
-    >
-      {children}
-    </NavLink>
-  </li>
+  <div className='w-52 mx-auto md:w-auto text-lg md:text-base'>
+    <li className='my-12'>
+      <NavLink
+        className='flex justify-between gap-2 items-center'
+        end
+        style={({ isActive }) => ({ color: isActive ? '#6A5ACD' : 'black' })}
+        to={to}
+      >
+        {children}
+      </NavLink>
+    </li>
+  </div>
 );
 
 const Navbar = () => {
@@ -42,22 +44,22 @@ const Navbar = () => {
         <NavLink to='/' end>
           <div className='flex gap-2'>
             <div>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
               </svg>
             </div>
-            <h2>Blog</h2>
+            <h2 className='text-lg md:text-base'>Blog</h2>
           </div>
         </NavLink>
         <div>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className={`w-6 h-6 md:hidden`} onClick={toggleMenu}>
-            <path fillRule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14Zm.75-10.25a.75.75 0 0 0-1.5 0v4.69L6.03 8.22a.75.75 0 0 0-1.06 1.06l2.5 2.5a.75.75 0 0 0 1.06 0l2.5-2.5a.75.75 0 1 0-1.06-1.06L8.75 9.44V4.75Z" clipRule="evenodd" />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 md:hidden" onClick={toggleMenu}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
           </svg>
         </div>
         <div>
-          <ul className={`${isMenuOpen ? 'flex-col gap-2 absolute w-full top-12 left-0 bg-zinc-50 border-b border-b-gray-300' : 'hidden'} md:flex md:space-x-8`}>
+          <ul className={`${isMenuOpen ? 'flex-col absolute w-full h-screen top-14 left-0 bg-zinc-50 border-b border-b-gray-300 opacity-95' : 'hidden'} md:flex md:space-x-8`}>
             {token ? <>
-              <NavItem to='/' className='bg-red-50'>
+              <NavItem to='/'>
                 <div>Home</div>
                 <div>
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
@@ -90,19 +92,20 @@ const Navbar = () => {
                 </div>
               </NavItem>
               <NavItem to='/user/me'>
+                <div>Profile</div>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-7 h-7">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                 </svg>
               </NavItem>
             </> : <></>}
-            <li className='my-2'>
+            <li className='flex justify-center'>
               {localStorage.getItem('auth-token')
-                ? <button onClick={() => { 
-                  localStorage.removeItem('auth-token'); 
-                  localStorage.removeItem('refresh-token'); 
+                ? <button onClick={() => {
+                  localStorage.removeItem('auth-token');
+                  localStorage.removeItem('refresh-token');
                   localStorage.removeItem('authUser');
                   window.location.replace('/');
-                }}>Logout</button>
+                }} className='text-lg md:text-base'>Logout</button>
                 : <NavLink
                   end
                   style={({ isActive }) => ({ color: isActive ? '#6A5ACD' : 'black' })}
